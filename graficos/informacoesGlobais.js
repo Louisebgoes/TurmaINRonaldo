409a0c770114566d2975fd7d45224cd5d9b65a97
Louisebgoes <115102707+Louisebgoes@users.noreply.github.com>
2024-10-24 12:13:52 +0000

    ajeitando

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,5 +4,12 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
+
 }
+
+
+
+
+
+
 vizualizarInformacoesGlobais()
